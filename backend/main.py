c8dd7db136bce85dd7fbd26438b74b2249ec98a9
Suyank Saraswat <suyank.saraswat@gmail.com>
2023-02-27 00:54:19 +0530

    supabase integration

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
+import json
 from fastapi import FastAPI
 from scraper import Scraper
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from supabase import create_client, Client
+from starlette.responses import Response
+
+# Add supabase url and key below
+url = ''
+key = ''
+
+supabase: Client = create_client(url, key)
 
 app = FastAPI()
 quotes = Scraper()
@@ -19,9 +28,25 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 class ScrapperReq(BaseModel):
     url: str
 
+
 @app.post("/scrapper")
 async def scrape_by_url(req: ScrapperReq):
-    return quotes.scrapedata(req)
+    scrape_data = supabase.table('scrape').select(
+        '*').filter('url', 'eq', req.url).execute()
+
+    if (len(scrape_data.data) > 0):
+        return str(scrape_data.data[0]['scrape_html'])
+
+    scrapped_data = quotes.scrapedata(req)
+
+    try:
+        supabase.table('scrape').insert(
+            {'url': req.url, 'scrape_html': str(scrapped_data)}).execute()
+    except:
+        pass
+
+    return scrapped_data
